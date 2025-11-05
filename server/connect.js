043f5391e59f6ec88d032a8507f37270873d1d84
utkarshdev2 <awesomeutk@gmail.com>
2025-11-05 15:20:36 -0500

    Revert "attempts to connect DB"
    This reverts commit 6184e5a17eb151345404b29215bf399c41df463e.

--- server/connect.js
+++ server/connect.js
@@ -1,18 +1,16 @@
 import dotenv from 'dotenv';
-dotenv.config({ path: './config.env' });
-import { MongoClient } from 'mongodb';
+dotenv.config({path: './config.env'})
+import { MongoClient } from "mongodb";
 
 const client = new MongoClient(process.env.ATLAS_URI);
 
-async function connectDB() {
-  try {
-    await client.connect();
-    const db = client.db('HuskyFilmFestival');
-    console.log('Connected to MongoDB Atlas');
-    return db;
-  } catch (err) {
-    console.error('MongoDB connection error:', err);
-  }
-}
+let db;
 
-export { connectDB };
+async function connectDB() {
+  if (!db) {
+    await client.connect();
+    db = client.db("HuskyFilmFestival");
+    console.log("Connected to MongoDB Atlas");
+  }
+  return db;
+}
--- testEnv.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-import path from "path";
-
-const envPath = path.resolve("./server/config.env");
-console.log("Looking for config.env at:", envPath);
-
-const result = dotenv.config({ path: envPath });
-console.log("Dotenv result:", result);
-console.log("ATLAS_URI:", process.env.ATLAS_URI);
