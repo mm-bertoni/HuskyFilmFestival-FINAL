552fd37fb805a4c8a061c0f07c1f89fb34b28d57
Margaret M Bertoni <bertoni.m@northeastern.edu>
2025-12-08 09:40:01 -0500

    Changing film counter, updated README with link

---
+++ frontend/src/assets/components/Film/totalFilmsSubmitted.jsx
@@ -0,0 +1,38 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import FilmStatAggregator from '../Film/filmStatAggregator';
+
+import {useState, useEffect} from 'react';
+
+export default function TotalFilmsSubmitted(){
+    const [filmCount, setFilmCount] = useState(0);
+
+  // To update the film count
+    const updateStat = async ()=>{
+        const res = await fetch(`/api/countFilms`);
+        if(!res.ok){
+            console.error("Failed to fetch sum");
+            res.status(500).json({ error: "Failed to get film count" });
+  
+    }
+    const data = await res.json();
+    //console.log("What am I getting for the sum", data.filmCount);
+    setFilmCount(data.filmCount); 
+
+  };
+
+  useEffect(()=>{
+    updateStat();
+  },[]);
+  
+  return (
+    <>
+        <Container>
+            <FilmStatAggregator
+                stat={filmCount}
+                type=""
+            />
+        </Container>
+    </>
+  );
+}
--- frontend/src/assets/components/pages/filmSubmissionPage.jsx
+++ frontend/src/assets/components/pages/filmSubmissionPage.jsx
@@ -1,32 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import FilmForm from "../Forms/filmForm";
-import FilmStatAggregator from '../Film/filmStatAggregator';
 import Navbar from '../Navbar'; 
 
-import {useState, useEffect} from 'react';
 
 export default function FilmSubmissionPage() {
-  const [filmCount, setFilmCount] = useState(0);
-
-  // To update the film count
-  const updateStat = async ()=>{
-    const res = await fetch(`/api/countFilms`);
-    if(!res.ok){
-      console.error("Failed to fetch sum");
-      res.status(500).json({ error: "Failed to get film count" });
   
-    }
-    const data = await res.json();
-    //console.log("What am I getting for the sum", data.filmCount);
-    setFilmCount(data.filmCount); 
 
-  };
-
-  useEffect(()=>{
-    updateStat();
-  },[]);
-  
   
   return (
     <>
@@ -34,12 +14,6 @@ export default function FilmSubmissionPage() {
       <Container>
         <h1>Submit Your Film</h1>
         <FilmForm />
-      </Container>
-      <Container>
-        <FilmStatAggregator
-          stat={filmCount}
-          type=""
-        />
       </Container>
     </>
   );
