0839189eef74ef2a76877c1879b3def8ea2df07b
utkarshdev2 <awesomeutk@gmail.com>
2025-12-08 13:24:48 -0500

    PropTypes defined

--- frontend/src/assets/components/Film/acceptedFilm.jsx
+++ frontend/src/assets/components/Film/acceptedFilm.jsx
@@ -1,28 +1,26 @@
 //import Container from 'react-bootstrap/Container';
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
 //import ReviewForm from '../Forms/reviewForm';
-import '../styles/acceptedFilm.css';
+import "../styles/acceptedFilm.css";
+import PropTypes from "prop-types";
 
-export default function FilmToReview({director, title, genre}){
-    return(
-            <Col>
-
-                <Card style={{width: '18rem'}} className="acceptedCard gy-1">
-                    <Card.Body>
-                        <Card.Title>{title}</Card.Title>
-                        <Card.Text className="cardText">
-                            Director: {director} 
-                        </Card.Text>
-                        <Card.Text className="cardText">
-                            Genre: {genre}
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-                
-            </Col>
-        
-
-    );
-
+export default function FilmToReview({ director, title, genre }) {
+  return (
+    <Col>
+      <Card style={{ width: "18rem" }} className="acceptedCard gy-1">
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text className="cardText">Director: {director}</Card.Text>
+          <Card.Text className="cardText">Genre: {genre}</Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
 }
+
+FilmToReview.propTypes = {
+  director: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  genre: PropTypes.string.isRequired,
+};
--- frontend/src/assets/components/Film/filmStatAggregator.jsx
+++ frontend/src/assets/components/Film/filmStatAggregator.jsx
@@ -1,12 +1,17 @@
-import Container from 'react-bootstrap/Container';
-import '../styles/filmStatAggregator.css' // Importing CSS styling
+import Container from "react-bootstrap/Container";
+import "../styles/filmStatAggregator.css"; // Importing CSS styling
+import PropTypes from "prop-types";
 
-export default function FilmStatAggregator({ stat="0", type=""}){
-    
-    return(
-        <Container className="statContainer">
-            <h2>Number of Films Submitted {type}: </h2>
-            <h3 className="statText">{stat}</h3>
-        </Container>
-    );
+export default function FilmStatAggregator({ stat = "0", type = "" }) {
+  return (
+    <Container className="statContainer">
+      <h2>Number of Films Submitted {type}: </h2>
+      <h3 className="statText">{stat}</h3>
+    </Container>
+  );
 }
+
+FilmStatAggregator.propTypes = {
+  stat: PropTypes.string,
+  type: PropTypes.string,
+};
