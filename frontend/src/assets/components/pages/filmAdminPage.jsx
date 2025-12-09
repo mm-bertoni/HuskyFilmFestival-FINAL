3bc1636c588bc67dc376b28438679c829c3d9e91
utkarshdev2 <awesomeutk@gmail.com>
2025-12-08 23:45:08 -0500

    accessibility

--- frontend/src/assets/components/pages/filmAdminPage.jsx
+++ frontend/src/assets/components/pages/filmAdminPage.jsx
@@ -1,25 +1,22 @@
-import { StrictMode } from 'react'
+import { StrictMode } from "react";
 //import { useState } from 'react';
 //import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap React for a Container
-import Container from 'react-bootstrap/Container';
+import Container from "react-bootstrap/Container";
 //import AdminForm from '../adminForm';
-import LoginAdmin from '../Film/logInAdmin';
-import Navbar from '../Navbar';
+import LoginAdmin from "../Film/logInAdmin";
+import Navbar from "../Navbar";
 
-export default function FilmAdminPage(){
-  return(
-  <StrictMode>
-    <Navbar/>
-    <Container>
-      <LoginAdmin/>
-    </Container>
-    
-    
-  </StrictMode>
-);
+export default function FilmAdminPage() {
+  return (
+    <StrictMode>
+      <Navbar />
+      <main>
+        <Container>
+          <LoginAdmin />
+        </Container>
+      </main>
+    </StrictMode>
+  );
 }
-
-
-
